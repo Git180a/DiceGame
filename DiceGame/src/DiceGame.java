60a4a0e72df170c4a269fd507ee99582a76be35d
Computer Science <computerscience@localadmins-MacBook-Air.local>
2023-11-02 09:03:59 -1000

    Added Languages

--- DiceGame/src/DiceGame.java
+++ DiceGame/src/DiceGame.java
@@ -3,40 +3,43 @@ import java.util.Scanner;
 
 public class DiceGame {
 	private static String[][] Language;
+	private static int Lang=0;
 
 	public static void main(String[] args) {
 		int die1;
 		int die2;
 		String[] playerChoices;
 		Language = initLang();
+		String langChoice = getInput(Language[Lang][10]);
+		Lang = Integer.parseInt(langChoice);
 		die1 = roll();
 		die2 = roll();
 		String choice;
-		choice = getInput(Language[0][0]);
+		choice = getInput(Language[Lang][0]);
 		choice = choice.toLowerCase();
 		int count = 0;
-		String playerCount = getInput(Language[0][1]);
+		String playerCount = getInput(Language[Lang][1]);
 		count = Integer.parseInt(playerCount);
 		playerChoices = new String[count];
-		while(choice.equals(Language[0][2])) {
+		while(choice.equals(Language[Lang][2])) {
 			
 			for(int c= 0; c < count; c++) {
-			playerChoices[c] = getInput(Language[0][3]+(c+1)+Language[0][4]);
+			playerChoices[c] = getInput(Language[Lang][3]+(c+1)+Language[Lang][4]);
 			
 			}
-			System.out.println(Language[0][5]+die1+Language[0][6]+die2);
+			System.out.println(Language[Lang][5]+die1+Language[Lang][6]+die2);
 			//Start a new loop here.
 			for(int c=0; c<count;c++) {
 				if(didIWin(die1,die2,playerChoices[c]))
 				{
-					System.out.println(Language[0][3]+(c+1)+Language[0][7]);
+					System.out.println(Language[Lang][3]+(c+1)+Language[Lang][7]);
 				}else
 				{
-					System.out.println(Language[0][3]+(c+1)+Language[0][8]);
+					System.out.println(Language[Lang][3]+(c+1)+Language[Lang][8]);
 				}
 			}
 			
-			choice = getInput(Language[0][9]);
+			choice = getInput(Language[Lang][9]);
 			choice = choice.toLowerCase();
 			die1 = roll();
 			die2 = roll();
@@ -45,8 +48,13 @@ public class DiceGame {
 	private static String[][] initLang() {
 		String [][] temp = new String[][] {
 			{"Do you want to play?\nYes\nNo","How many players are there?","yes",
-				"Player ",": [0] Even\n[1] Odd","Die 1: ","\nDie 2: "," wins!",
-				" loses!","Do you want to play again?\nYes\nNo"}
+				"Player ",": [0]Even\n[1]Odd","Die 1: ","\nDie 2: "," wins!",
+				" loses!","Do you want to play again?\nYes\nNo","Enter Language \nEnglish[0]\nHawaiian Pigeon[1]"
+			},
+			{"Like try?\nYessah\nNo","How many dakine Braddahs?","yessah","Bradda ",
+				": [0]Even\n[1]Odd","Die 1: ","\nDie 2: "," Winnah"," Losah",
+				"Like try again?\nYessah\nNo","Enter Language \nEnglish[0]\nHawaiian Pigeon[1]"
+			}
 			
 			
 			};
